273bc0e2585ed6d4282043fb349b49bcff9c388f
Krunal Yadav <krunal.yadav18@gmail.com>
2018-03-06 23:58:48 +0530

    Create custom and private routes

---
+++ src/components/logging/LoggingDefaultMessage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LoggingDefaultMessage = () => {
+
+    return (
+        <div>
+            <h3>Front Matter</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris tincidunt, dui ac condimentum pharetra, massa ligula pretium enim, nec dictum ligula orci quis mauris. Maecenas in malesuada ante. Etiam auctor tellus nec metus suscipit, at malesuada elit dictum. Vivamus tellus augue, pulvinar sed interdum id, suscipit eu arcu. Duis volutpat condimentum auctor. Nulla facilisi. Nullam tincidunt lobortis pharetra.
+            </p>
+        </div>
+    );
+
+};
+
+export default LoggingDefaultMessage;
---
+++ src/components/logging/LoggingLink.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Link, withRouter} from "react-router-dom";
+
+class LoggingLink extends React.Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    render = () => {
+
+        return (
+            <Link {...this.props}
+                    onClick={this.handleClick}
+            >{this.props.children}</Link>
+        );
+    };
+
+    handleClick = () => {
+
+        let urlParts = this.props.to.split('/');
+        const eid = urlParts[urlParts.length-1];
+
+        alert('Clicking the Link is Logging: ' + eid);
+    };
+}
+
+LoggingLink.propTypes = {
+    to: PropTypes.string.isRequired
+};
+
+export default withRouter(LoggingLink);
