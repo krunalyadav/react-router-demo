c3ca54817e43d52473fd84e10c893137c64e3818
Krunal Yadav <krunal.yadav18@gmail.com>
2018-03-08 19:29:36 +0530

    Add route effects

--- src/components/colors/SmartColorSwatch.js
+++ src/components/colors/SmartColorSwatch.js
@@ -1,21 +1,28 @@
 import React from 'react';
-import {withRouter} from "react-router-dom";
+import { withRouter, Link, Route } from "react-router-dom";
 
-const SmartColorSwatch = ({match}) => {
+const SmartColorSwatch = ({ match }) => {
 
-    let style={
+    let style = {
         backgroundColor: match.params.color,
         textAlign: 'center',
         padding: 20,
         borderColor: '#000000',
         borderStyle: 'solid',
-        margin:15
+        margin: 15
     };
 
     return (
         <div style={style}>
             <h2>{match.params.text}</h2>
             <h3>{match.path}</h3>
+            <h4>
+                <Link to={`${match.url}/ececec/Child`}>Add Child</Link>
+                {' | '}
+                <Link to={`${match.url}`}>Close Child</Link>
+            </h4>
+
+            <Route path={`${match.url}/:color/:text`} component={SmartColorSwatch} />
         </div>
     );
 
